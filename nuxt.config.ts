197233c27a998bff4144d4366fda5a9da0ace56f
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-02-22 10:48:36 +0530

    change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,7 @@ import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
     devtools: { enabled: false },
-    app: {
-        head: {
-          title: 'testing', // Set your website name here
-          meta: [
-            { name: 'description', content: 'Description of your website' },
-            // Add other meta tags as needed
-          ],
-        },
-      },
+   
     css: ['@/assets/css/global.css'],
     modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
     primevue: {
