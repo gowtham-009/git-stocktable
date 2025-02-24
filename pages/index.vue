f93dacc891c3edd271dff0dfa368e4f44991eb39
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-02-24 09:59:09 +0530

    clientcode change

--- pages/index.vue
+++ pages/index.vue
@@ -135,7 +135,7 @@
                   src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                   alt="" />
                 <span class="hidden lg:flex lg:items-center">
-                  <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true">{{ val }}</span>
+                  <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true">{{ clientcode }}</span>
                   <ChevronDownIcon class="ml-2 w-10 h-10 text-gray-400" aria-hidden="true" />
                 </span>
               </MenuButton>
